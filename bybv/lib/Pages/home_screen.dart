7675f0ff73fc8db082186cb9f1c3088260d1b56c
alessiogatto04 <alessio.gatto13032004@gmail.com>
2025-11-15 17:38:53 +0100

    Definizione base home_screen

--- bybv/lib/Pages/home_screen.dart
+++ bybv/lib/Pages/home_screen.dart
@@ -4,10 +4,25 @@ class HomeScreen extends StatelessWidget{
   const HomeScreen({
     super.key
   })
-
+  
   @override
   Widget build(BuildContext context){
+    return Stack(
+      //videoPlayer
+      children: <Widget> [
+        Positioned(
+          top: 75,
+          left: 50,
+          child: Text("BYBV",
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
 
+      ]
+    );
     
 
 
