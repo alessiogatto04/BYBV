8eb79cb539d0c34fd2f51a640bf5ac688bb8cac2
antoleaf <fglnnn04l30d086f@studenti.unical.it>
2025-11-15 15:17:37 +0100

    modifica bottone

--- bybv/lib/Widget/Login_Button.dart
+++ bybv/lib/Widget/Login_Button.dart
@@ -2,19 +2,33 @@ import 'package:flutter/material.dart';
 
 class LoginButton extends StatelessWidget{
   final String text; // è final perchè è StatelessWidget
-  final VoidCallbackAction onPressed;  //VoidCallBack è una classe che specifica che il cliccare un bottone non effettua nessuan funzione con ritorno
+  final VoidCallback onPressed;  //VoidCallBack è una classe che specifica che il cliccare un bottone non effettua nessuan funzione con ritorno
 
-  const LoginButton({super.key, required this.text});
-  // SUper.key serve alla classe superiore per accedere a questo nel widget tree
-  //required serve per definire che la variabile , in questo caso this.text , è strettamente necessaria da inserire
+  const LoginButton({
+    super.key,          // Super.key serve alla classe superiore per accedere a questo nel widget tree
+    required this.text,
+    required this.onPressed
+  });
+  //required serve per definire che la variabile , in questo caso this.text e onPressed, sono strettamente necessaria da inserire
 
   @override
   Widget build(BuildContext context){ // build serve a definire qualcosa di nuovo che stiamo creiamo . Memtre buildContext serve sempre per definire 
   //dove si trova nella gerarchia dell'albero dei context widget
-    return MaterialApp( //material app definisce la grafica del nostro bottone attraverso Scaffold
-    home: Scaffold(
-      
+    return OutlinedButton(    // return OutlinedButton(onPressed: onPressed, child: child) --> si aspetta questi valori, ovvero onPressed --> cosa succede quando clicco il bottone. child --> definisce cosa c'è dentro il bottone (TEXT)
+      onPressed: onPressed, 
+      style: OutlinedButton.styleFrom(
+        backgroundColor: Colors.transparent,
+        foregroundColor: Colors.black,
+        side: const BorderSide(color: Colors.black, width: 2),
+        shape: RoundedRectangleBorder(      // con shape definiamo la forma di un bottone, nel nostro caso un rettangoo con bordi modificabili e arrotondati di 8 pixel
+          borderRadius: BorderRadius.circular(8),
+        )
+      ),
+      child: Text(
+        text,
+        style: const TextStyle(fontSize: 12, fontWeight: FontWeight.bold)
       )
     );
+
   }
 }
