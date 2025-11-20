1636b80514588cf5d49709ee79ea6f683bfb7822
candre11 <dzdq7js88k@privaterelay.appleid.com>
2025-11-20 13:24:06 +0100

    Aggiunto l'utilizzo di MediaQuery per adattamento ai devices

--- bybv/lib/Pages/home_screen.dart
+++ bybv/lib/Pages/home_screen.dart
@@ -1,61 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:bybv/Widget/Acces_Login_Button.dart';
+import 'package:flutter/services.dart';
 
 class HomeScreen extends StatelessWidget{
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context){
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(  
       backgroundColor: Colors.black,  // Puoi cambiare il colore
       body: Stack(
         children: <Widget> [
           Positioned(
-            top: 75,
-            left: 50,
+            top: screenHeight *0.1,
+            left: screenWidth *0.1,
             child: Text(
               "BYBV",
               style: TextStyle(
                 color: Colors.blue,
                 fontWeight: FontWeight.bold,
-                fontSize: 24,
+                fontSize: screenWidth *0.06,
               ),
             ),
           ),
           
           Positioned(
-            bottom: 100,
+            bottom: screenHeight *0.12,
             left: 0,
             right: 0,
             child:Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 SizedBox(
-                  width:150,
-                  height:60,
+                  width:screenWidth *0.40,
+                  height:screenHeight *0.075,
                   child: A_L_Button(
                     text: Text(
                       "Register",
                       style: TextStyle(
                         color: Colors.white,
                         fontWeight: FontWeight.bold,
-                        fontSize: 18,
+                        fontSize: screenWidth *0.045,
                       ),
                     ),
                     onPressed: (){},
                   ),
                 ),
-                SizedBox(width: 30), // Regola questo numero per lo spazio che vuoi
+                SizedBox(width: screenWidth *0.08), // Regola questo numero per lo spazio che vuoi
                 SizedBox(
-                  width:150,
-                  height:60,
+                  width:screenWidth *0.40,
+                  height:screenHeight *0.075,
                   child: A_L_Button (
                     text: Text(
                       "Login",
                       style: TextStyle(
                         color: Colors.white,
                         fontWeight: FontWeight.bold,
-                        fontSize: 18,
+                        fontSize: screenWidth *0.045,
                       ),
                     ),
                     onPressed: (){},
